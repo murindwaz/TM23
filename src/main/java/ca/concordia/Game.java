6c7e9e4146e4143993d9e36dc517cd3cb7183b1c
Pascal Maniraho <murindwaz@yahoo.fr>
2015-01-27 21:56:46 -0500

    Converted the project to a maven project + added basic test classes

---
+++ src/main/java/ca/concordia/Game.java
@@ -0,0 +1,12 @@
+package ca.concordia;
+
+
+
+
+/**
+ * This is temporal entry point. 
+ * @author root
+ */
+public class Game {
+
+}
---
+++ src/test/java/ca/concordia/game/FristTest.java
@@ -0,0 +1,5 @@
+package ca.concordia.game;
+
+public class FristTest {
+
+}
