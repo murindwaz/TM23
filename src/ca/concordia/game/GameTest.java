e2a917f4a4cef87adf77394b4aee1ce78e66f065
Pascal Maniraho <murindwaz@yahoo.fr>
2015-03-09 23:46:48 -0400

    added missing test files

---
+++ src/ca/concordia/game/GameTest.java
@@ -0,0 +1,21 @@
+package ca.concordia.game;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import ca.concordia.game.main.*;
+/**
+ * 
+ * Class GameTest JUnit. Test functionality of initializing a new game.
+ *
+ */
+
+public class GameTest {
+	
+	/**
+	 * Create a new Game
+	 */
+	
+	
+	
+}
---
+++ test/ca/concordia/game/AllTests.java
@@ -0,0 +1,18 @@
+package ca.concordia.game;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * 
+ * TEst all four Test Cases.
+ *
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({ GameTest.class, GameTest2.class, GameTest3.class,
+		GameTest4.class })
+public class AllTests {
+
+}
