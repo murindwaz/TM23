5502a2ac14fb9c9bc0cf07491d3bd7698c1c49d3
Pascal Maniraho <murindwaz@yahoo.fr>
2015-03-12 13:19:21 -0400

    updated the tests by removing deck of type C

--- test/ca/concordia/game/GameModelTest.java
+++ test/ca/concordia/game/GameModelTest.java
@@ -18,7 +18,7 @@ public class GameModelTest {
 	@Test public void deck(){
 		assertNotNull( "The discard deck is empty to start with", new Deck(new String("D"), 2) );
 		assertNotNull( "The personality deck has 7 cards:", new Deck(new String("P"), 2) );
-		assertNotNull( "The city deck has 12 cards:", new Deck(new String("C"), 2) );
+		//assertNotNull( "The city deck has 12 cards:", new Deck(new String("C"), 2) );
 		assertNotNull( "The event deck has 12 cards:", new Deck(new String("E"), 2) );
 		assertNotNull( "The brown-bordered player deck has 48 cards:", new Deck(new String("B"), 2) );
 		assertNotNull( "The green-bordered player deck has 53 cards:", new Deck(new String("G"), 2) );
