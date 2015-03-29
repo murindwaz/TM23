b758f1956cecd9c7daa80bf60847a478ac169b96
Pascal Maniraho <murindwaz@yahoo.fr>
2015-03-29 15:27:28 -0400

    Added testing for States initialiazation

--- test/ca/concordia/game/GameUtilTest.java
+++ test/ca/concordia/game/GameUtilTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ca.concordia.game.gameState.*;
+import ca.concordia.game.util.Configuration;
 
 public class GameUtilTest {
 	
@@ -16,11 +17,12 @@ public class GameUtilTest {
 		StatePlay statePlay = new StatePlay();
 		StateDrawCard stateDrawCard = new StateDrawCard();
 		StateWait stateWait = new StateWait();
-		//StateContext stateContext = new StateContext();
 		assertTrue( "StatePlay is type of StateLike ", statePlay instanceof StateLike ); 
 		assertTrue( "StateDrawCard is type of StateLike ", stateDrawCard instanceof StateLike ); 
 		assertTrue( "StateWait is type of StateLike ", stateWait instanceof StateLike ); 
-		//assertTrue( "StateContext is type of StateLike ", stateContext instanceof StateLike ); 
+		assertEquals("StatePlay has Playing status", statePlay.getStatus(), Configuration.STATE_PLAY);
+		assertEquals("StateDrawingCard has Drawing status", stateDrawCard.getStatus(), Configuration.STATE_DRAWING);
+		assertEquals("StateWait has waiting status", stateWait.getStatus(), Configuration.STATE_WAIT);
 	}
 
 
